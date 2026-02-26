ee515bf05f52ef10e801de62bdc036fdf8e0f13c
Divyansh Raj Tripathi <drt.vip777@gmail.com>
2026-02-26 21:45:58 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-purple-500">
+        OneTool Setup Complete 🚀
+      </h1>
+    </main>
+  );
+}
